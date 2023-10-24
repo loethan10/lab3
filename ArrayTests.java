80f9cef2ee25c8226f95b084b3b3407583e40567
Ethan Lo <loethan101@gmail.com>
2023-10-24 09:03:03 -0700

    Lab 4

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+	public void testReverseInPlaceAgain() {
+    int[] input1 = {3, 4, 5, 6, 7};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{7, 6, 5, 4, 3}, input1);
+	}
+
+
+  @Test
+  public void testReversedAgain() {
+    int[] input1 = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  }
 }
